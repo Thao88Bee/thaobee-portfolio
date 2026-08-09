c1971ab39e336e6e01723cd2e63f16aab391fd74
Thao88bee <beethao.bt@gmail.com>
2026-08-09 05:35:46 -0500

    Add Users table migration and seeds

---
+++ backend/db/seeders/1-20260809100528-demo-user.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await User.bulkCreate(
+      [
+        {
+          username: "Demo-user",
+          firstname: "Demo",
+          lastname: "User",
+          email: "demo@user.io",
+          hashedPassword: bcrypt.hashSync("password"),
+        },
+      ],
+      { validate: true },
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      options,
+      {
+        username: { [Op.in]: ["Demo-user"] },
+      },
+      {},
+    );
+  },
+};
