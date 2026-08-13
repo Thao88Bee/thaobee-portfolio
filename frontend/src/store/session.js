bda902a52acc854c3f13ee5ed0aab28899cb4ddb
Thao88bee <beethao.bt@gmail.com>
2026-08-13 06:42:12 -0500

    Setup signup thunk action

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -16,6 +16,23 @@ const removeUser = () => {
   };
 };
 
+export const signup = (user) => async (dispatch) => {
+  const { username, firstname, lastname, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      firstname,
+      lastname,
+      email,
+      password,
+    }),
+  });
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
   const response = await csrfFetch("/api/session", {
