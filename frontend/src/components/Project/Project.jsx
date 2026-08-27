b01ecec7c01d8291cd60853cc01086c699a02df6
Thao88bee <beethao.bt@gmail.com>
2026-08-26 22:34:06 -0500

    Project Page Added

--- frontend/src/components/Project/Project.jsx
+++ frontend/src/components/Project/Project.jsx
@@ -1,7 +1,26 @@
+import poultryCenter from "/poultry-center.ico";
+
 import "./Project.css";
 
 function Project() {
-  return <div>Project</div>;
+  return (
+    <section className="projectSection">
+      <div className="poultryCenter">
+        <a
+          href="https://poultrycenter.onrender.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={poultryCenter} alt="Poultry Center Logo" />
+        </a>
+        <h2>Poultry Center</h2>
+        <p>
+          Web application all about poultry and poultry lovers sharing
+          idea&apos;s and concern&apos;s or just fun facts.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default Project;
