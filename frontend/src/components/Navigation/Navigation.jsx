169597fcfec891175912dc56bc5eb6152739f1b4
Thao88bee <beethao.bt@gmail.com>
2026-08-28 10:00:09 -0500

    Light/Dark Theme Added

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,16 +1,32 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import ProfileButton from "./ProfileButton";
+import { ThemeContext } from "../../common/ThemeContext";
+import { PiSunFill } from "react-icons/pi";
+import { PiMoonFill } from "react-icons/pi";
+
 import "./Navigation.css";
 
 function Navigation({ isLoaded }) {
+  const useTheme = () => useContext(ThemeContext);
+  const { theme, toggleTheme } = useTheme();
+
   const sessionUser = useSelector((state) => state.session.user);
+
+  const themeIcon =
+    theme === "light" ? (
+      <PiSunFill onClick={toggleTheme} />
+    ) : (
+      <PiMoonFill className="lightMoon" onClick={toggleTheme} />
+    );
 
   return (
     <>
       {!sessionUser ? (
         <div className="navBar">
           <div className="navLinkContainer">
+            {themeIcon}
             <NavLink className="navLink" to={""}>
               About
             </NavLink>
@@ -27,7 +43,8 @@ function Navigation({ isLoaded }) {
           <div>{isLoaded && <ProfileButton user={sessionUser} />}</div>
         </div>
       ) : (
-        <div className="LoginNavBar">
+        <div className="navBar">
+          {themeIcon}
           <div>{isLoaded && <ProfileButton user={sessionUser} />}</div>
         </div>
       )}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
 import { Provider } from "react-redux";
+import App from "./App";
 import configureStore from "./store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
 import { Modal, ModalProvider } from "./context/Modal";
+import { ThemeProvider } from "./common/ThemeContext";
+
+import "./index.css";
 
 const store = configureStore();
 
@@ -22,7 +24,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ModalProvider>
       <Provider store={store}>
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
         <Modal />
       </Provider>
     </ModalProvider>
