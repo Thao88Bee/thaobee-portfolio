3edc5ebcd1f7a4de2a784dee34fa57cabf31ccb0
Thao88bee <beethao.bt@gmail.com>
2026-09-01 22:14:57 -0500

    Contact Page

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -1,7 +1,34 @@
 import "./Contact.css";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <section className="contactSection">
+      <h1>How To Contact Me?</h1>
+      <p>By Phone: (920) 242-4062</p>
+      <h2>Message Me on Here</h2>
+      <div className="formContainer">
+        <form>
+          <label>
+            First Name:
+            <input type="text" />
+          </label>
+          <label>
+            Last Name:
+            <input type="text" />
+          </label>
+          <label>
+            Email:
+            <input type="text" />
+          </label>
+          <label>
+            Message:
+            <textarea></textarea>
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
