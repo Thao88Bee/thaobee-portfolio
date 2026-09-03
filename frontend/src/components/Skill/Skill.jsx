70ac14f19d211f6c8fe03b39b87cae4d4a9d94ff
Thao88bee <beethao.bt@gmail.com>
2026-09-03 08:10:44 -0500

    Added icon for language in Skill Page

--- frontend/src/components/Skill/Skill.jsx
+++ frontend/src/components/Skill/Skill.jsx
@@ -1,15 +1,36 @@
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiPython } from "react-icons/si";
+import { TbSql } from "react-icons/tb";
+import { ImHtmlFive } from "react-icons/im";
+import { IoLogoCss3 } from "react-icons/io";
+
 import "./Skill.css";
 
 function Skill() {
   return (
     <section className="skillSection">
       <h2>Language</h2>
-      <div className="skillList">
-        <p>Javascript</p>
-        <p>Python</p>
-        <p>SQL</p>
-        <p>HTML</p>
-        <p>CSS</p>
+      <div className="languageList">
+        <div className="icon">
+          <IoLogoJavascript className="javascriptIcon" />
+          <p>Javascript</p>
+        </div>
+        <div className="icon">
+          <SiPython className="pythonIcon" />
+          <p>Python</p>
+        </div>
+        <div className="icon">
+          <TbSql className="sqlIcon" />
+          <p>SQL</p>
+        </div>
+        <div className="icon">
+          <ImHtmlFive className="htmlIcon" />
+          <p>HTML</p>
+        </div>
+        <div className="icon">
+          <IoLogoCss3 className="cssIcon" />
+          <p>CSS</p>
+        </div>
       </div>
       <h2>Technology</h2>
       <div className="skillList">
